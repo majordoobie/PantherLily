f7a127b0b98f16d273728bf4fb9640e51a5f6f26
majordoobie <sgtmajordoobie@live.com>
2019-10-05 03:15:45 -0400

    Forgot to send feedback

--- pantherlily.py
+++ pantherlily.py
@@ -631,6 +631,9 @@ async def legend(ctx, *, user=None):
     async with ctx.typing():
         player_tag = query_result[0][0]
         player = await coc_client2.get_player(player_tag)
+        if player.legend.id != 29000022:
+            await ctx.send("Must be in legends to use me")
+            return
         seasons_list = await coc_client2.get_seasons(player.league.id)
         prev, prev2, prev3, prev4 = (seasons_list[-1]['id'], seasons_list[-2]['id'], seasons_list[-3]['id'], seasons_list[-4]['id'] )
         
