43640f8ab55cc6d4aa055633a028b105325a8ea3
majordoobie <Sgtmajordoobie@live.com>
2020-11-28 21:45:51 -0500

    Needs work - can't control when to stop the scheduled task. Need to change the logic around so that I can safely close down the database pool

--- database/update_daemon.py
+++ database/update_daemon.py
@@ -5,6 +5,7 @@ any slowdowns from I/O.
 import asyncio
 import asyncpg
 import coc
+from datetime import datetime
 import logging
 
 from packages.private.settings import Settings
@@ -16,10 +17,12 @@ WHERE is_active = 'true'
   and discord_user.discord_id = clash_account.discord_id
   and clash_account.is_primary_account = 'true'"""
 
-SQL_UPDATE_CLASSIC = """"""
+SQL_UPDATE_CLASSIC = """INSERT INTO clash_classic_update (increment_date, tag, current_donations) VALUES (?, ?, ?);"""
 
 async def update_active_users(sleep_time: int, coc_client: coc.client.Client, pool: asyncpg.pool.Pool, log: logging.Logger):
     while True:
+        log.debug("Running classic update daemon")
+
         async with pool.acquire() as conn:
             # Get all active members from the database
             active_members = await conn.fetch(SQL_GET_ACTIVE)
@@ -28,31 +31,31 @@ async def update_active_users(sleep_time: int, coc_client: coc.client.Client, po
             active_players = []
             async for player in coc_client.get_players((member['clash_tag'] for member in active_members)):
                 active_players.append(player)
+                print(player.get_achievement("Friend in Need").value)
+                print("FUCKKKKK")
+                return
 
             # Update classic table
             for player in active_players:
-                #TODO: Create the classic update
-
-
-
-
-
-
+                await conn.execute(SQL_UPDATE_CLASSIC, (
+                    datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S'),
+                    player.tag,
+                    player.get_achievement("Friend in Need").value,
+                ))
         await asyncio.sleep(sleep_time)
 
-def main():
+async def main():
     settings = Settings(daemon=True)
     loop = None
+    LoggerSetup(settings)
+    log = logging.getLogger('daemon.update')
+    loop = asyncio.get_event_loop()
     try:
-        LoggerSetup(settings)
-        log = logging.getLogger('daemon.update')
-        loop = asyncio.get_event_loop()
-
         # get pool and coc objects
         pool: asyncpg.pool.Pool = loop.run_until_complete(asyncpg.create_pool(settings.dsn))
-        coc_client: coc.client.Client = coc.login(settings.coc_user, settings.coc_pass,
-                                                  loop=loop, key_names="Panther Daemon")
-
+        coc_client: coc.client.Client = coc.login(settings.coc_user,
+                                                  settings.coc_pass,
+                                                  key_names="Panther Daemon")
         # Create the update task
         loop.create_task(update_active_users(
             15,
@@ -61,6 +64,7 @@ def main():
             log
         ))
         loop.run_forever()
+
     except KeyboardInterrupt:
         pass
     finally:
@@ -68,4 +72,4 @@ def main():
             loop.close()
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
