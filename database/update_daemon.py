03870d6b3a3deec6afeb98a8cc1e3a40be535d06
majordoobie <Sgtmajordoobie@live.com>
2020-11-29 14:21:55 -0500

    Move the logic around that have the started be a coro instead of a sync function.

--- database/update_daemon.py
+++ database/update_daemon.py
@@ -19,57 +19,93 @@ WHERE is_active = 'true'
 
 SQL_UPDATE_CLASSIC = """INSERT INTO clash_classic_update (increment_date, tag, current_donations) VALUES (?, ?, ?);"""
 
-async def update_active_users(sleep_time: int, coc_client: coc.client.Client, pool: asyncpg.pool.Pool, log: logging.Logger):
-    while True:
-        log.debug("Running classic update daemon")
+async def update_active_users(sleep_time: int, coc_client: coc.client.Client, pool: asyncpg.pool.Pool):
+    LOG.debug("Running classic update daemon")
+    async with pool.acquire() as conn:
+        # Get all active members from the database
+        active_members = await conn.fetch(SQL_GET_ACTIVE)
 
-        async with pool.acquire() as conn:
-            # Get all active members from the database
-            active_members = await conn.fetch(SQL_GET_ACTIVE)
+        # Get the coc object of each player
+        active_players = []
+        async for player in coc_client.get_players((member['clash_tag'] for member in active_members)):
+            active_players.append(player)
+            print(player.get_achievement("Friend in Need").value)
+            print("FUCKKKKK")
+            return
 
-            # Get the coc object of each player
-            active_players = []
-            async for player in coc_client.get_players((member['clash_tag'] for member in active_members)):
-                active_players.append(player)
-                print(player.get_achievement("Friend in Need").value)
-                print("FUCKKKKK")
-                return
+        # Update classic table
+        for player in active_players:
+            await conn.execute(SQL_UPDATE_CLASSIC, (
+                datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S'),
+                player.tag,
+                #player.get_achievement("Friend in Need").value,
+                10
+            ))
 
-            # Update classic table
-            for player in active_players:
-                await conn.execute(SQL_UPDATE_CLASSIC, (
-                    datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S'),
-                    player.tag,
-                    player.get_achievement("Friend in Need").value,
-                ))
-        await asyncio.sleep(sleep_time)
-
-async def main():
-    settings = Settings(daemon=True)
-    loop = None
-    LoggerSetup(settings)
-    log = logging.getLogger('daemon.update')
-    loop = asyncio.get_event_loop()
+async def test(pool, msg):
     try:
-        # get pool and coc objects
-        pool: asyncpg.pool.Pool = loop.run_until_complete(asyncpg.create_pool(settings.dsn))
-        coc_client: coc.client.Client = coc.login(settings.coc_user,
-                                                  settings.coc_pass,
-                                                  key_names="Panther Daemon")
-        # Create the update task
-        loop.create_task(update_active_users(
-            15,
-            coc_client,
-            pool,
-            log
-        ))
-        loop.run_forever()
+        while True:
+            print(msg)
+            async with pool.acquire() as conn:
+                print(len(await conn.fetch("select * from discord_user;")))
+                print("THis is that other one")
 
+            await asyncio.sleep(10)
     except KeyboardInterrupt:
-        pass
-    finally:
-        if loop:
-            loop.close()
+        print("THis is the control c doe")
+
+async def main(coc_client_):
+    pool : asyncpg.pool.Pool = await asyncpg.create_pool(SETTINGS.dsn)
+
+    async with pool.acquire() as conn:
+        print(len(await conn.fetch("select * from discord_user;")))
+        print("that is how many we got")
+
+    loop = asyncio.get_running_loop()
+    tasks = [
+        loop.create_task(test(pool, "This is the fist one")),
+        loop.create_task(test(pool, "This is the second one"))
+    ]
+    await asyncio.wait(tasks)
+
+
+   # Create tasks
+   # loop.create_task(update_active_users(
+   #     15,
+   #     coc_client,
+   #     pool
+   # ))
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    SETTINGS = Settings(daemon=True)
+    LoggerSetup(SETTINGS)
+    LOG = logging.getLogger('daemon.update')
+
+    _coc_client: coc.client.Client = coc.login(
+        SETTINGS.coc_user,
+        SETTINGS.coc_pass,
+        key_names="Panther Daemon",
+    )
+    asyncio.run(main(_coc_client))
+
+"""
+    _loop = None
+    _coc = None
+
+    try:
+        LOG.debug("Creating event loop and running main coroutine")
+        
+        _loop = asyncio.get_event_loop()
+
+        _loop.run_until_complete(main(_coc))
+    except KeyboardInterrupt:
+        print("Keyboard interrupt")
+    except Exception as error:
+        print(error)
+        print("\n\nWe here\n\n")
+        LOG.error(error, stack_info=True)
+    finally:
+        if _loop:
+            _coc.close()
+            _loop.close()
+            """
