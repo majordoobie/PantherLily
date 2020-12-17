ff3e80bb888a11feb83acaa7690e3722dba5cefe
majordoobie <Sgtmajordoobie@live.com>
2020-12-17 09:13:46 -0500

    Got basic updates working. Also notices that Zag has the highest at 800k so there is still 1.2M left to play with. Going to ignore the new donation update so I can push this out then come back to it

--- database/update_daemon.py
+++ database/update_daemon.py
@@ -3,6 +3,7 @@ This service will be used to automatically update the database with fresh inform
 any slowdowns from I/O.
 """
 import asyncio
+import nest_asyncio
 import asyncpg
 import coc
 from datetime import datetime
@@ -21,91 +22,54 @@ SQL_UPDATE_CLASSIC = """INSERT INTO clash_classic_update (increment_date, tag, c
 
 async def update_active_users(sleep_time: int, coc_client: coc.client.Client, pool: asyncpg.pool.Pool):
     LOG.debug("Running classic update daemon")
-    async with pool.acquire() as conn:
-        # Get all active members from the database
-        active_members = await conn.fetch(SQL_GET_ACTIVE)
-
-        # Get the coc object of each player
-        active_players = []
-        async for player in coc_client.get_players((member['clash_tag'] for member in active_members)):
-            active_players.append(player)
-            print(player.get_achievement("Friend in Need").value)
-            print("FUCKKKKK")
-            return
-
-        # Update classic table
-        for player in active_players:
-            await conn.execute(SQL_UPDATE_CLASSIC, (
-                datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S'),
-                player.tag,
-                #player.get_achievement("Friend in Need").value,
-                10
-            ))
-
-async def test(pool, msg):
     try:
         while True:
-            print(msg)
+            # Get all active members
             async with pool.acquire() as conn:
-                print(len(await conn.fetch("select * from discord_user;")))
-                print("THis is that other one")
+                active_members = await conn.fetch(SQL_GET_ACTIVE)
 
-            await asyncio.sleep(10)
+                async for player in coc_client.get_players((member['clash_tag'] for member in active_members)):
+                    await conn.execute(SQL_UPDATE_CLASSIC, (
+                        datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S'),
+                        player.tag,
+                        player.get_achievement("Friend in Need").value
+                    ))
+            await asyncio.sleep(sleep_time)
+
     except KeyboardInterrupt:
         print("THis is the control c doe")
+    except Exception as error:
+        import traceback
+        exc = ''.join(traceback.format_exception(type(error), error, error.__traceback__, chain=True))
+        print(exc)
+
+
 
 async def main(coc_client_):
     pool : asyncpg.pool.Pool = await asyncpg.create_pool(SETTINGS.dsn)
-
-    async with pool.acquire() as conn:
-        print(len(await conn.fetch("select * from discord_user;")))
-        print("that is how many we got")
-
     loop = asyncio.get_running_loop()
+
+    __coc_client_: coc.client.Client = coc.login(
+        SETTINGS.coc_user,
+        SETTINGS.coc_pass,
+        key_names="Panther Daemon",
+    )
+
     tasks = [
-        loop.create_task(test(pool, "This is the fist one")),
-        loop.create_task(test(pool, "This is the second one"))
+        loop.create_task(update_active_users(10, coc_client_, pool))
     ]
     await asyncio.wait(tasks)
 
-
-   # Create tasks
-   # loop.create_task(update_active_users(
-   #     15,
-   #     coc_client,
-   #     pool
-   # ))
 
 if __name__ == '__main__':
     SETTINGS = Settings(daemon=True)
     LoggerSetup(SETTINGS)
     LOG = logging.getLogger('daemon.update')
 
+    nest_asyncio.apply()
     _coc_client: coc.client.Client = coc.login(
         SETTINGS.coc_user,
         SETTINGS.coc_pass,
         key_names="Panther Daemon",
     )
     asyncio.run(main(_coc_client))
-
-"""
-    _loop = None
-    _coc = None
-
-    try:
-        LOG.debug("Creating event loop and running main coroutine")
-        
-        _loop = asyncio.get_event_loop()
-
-        _loop.run_until_complete(main(_coc))
-    except KeyboardInterrupt:
-        print("Keyboard interrupt")
-    except Exception as error:
-        print(error)
-        print("\n\nWe here\n\n")
-        LOG.error(error, stack_info=True)
-    finally:
-        if _loop:
-            _coc.close()
-            _loop.close()
-            """
