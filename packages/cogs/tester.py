1a1bb919e323118e4d778ddb23e3a52fdd9397ec
Jay Galindez <sgtmajordoobie@live.com>
2022-08-11 23:27:03 -0400

    [Added paginator.py] Created a paginator class using the examples from disnake. Just need to see if I should add it to the inter_send or not

--- packages/cogs/tester.py
+++ packages/cogs/tester.py
@@ -1,8 +1,11 @@
+from typing import List
+
 import asyncpg
 from disnake.ext import commands
 import disnake
 import asyncio
 
+from packages.utils.paginator import Paginator
 from packages.utils.utils import is_leader
 import packages.utils.bot_sql as sql
 
@@ -18,140 +21,30 @@ class Tester(commands.Cog):
         await self.bot.send(ctx=ctx, description='ponnnnnnnnng')
 
     @commands.slash_command(auto_sync=True)
-    async def sync_thing(self, inter):
-
-        guild = self.bot.get_guild(293943534028062721)
-        member_ids = {member.id: member for member in guild.members}
-
-        async with self.bot.pool.acquire() as conn:
-            conn: asyncpg.Connection
-            users = await conn.fetch(sql.select_discord_users(),
-                                     list(member_ids.keys()))
-
-            member: disnake.Member
-            for user in users:
-                if user["discord_id"] != 265368254761926667:
-                    continue
-
-                if not (member := member_ids.get(user["discord_id"])):
-                    continue
-
-                update_user = False
-                if member.name != user["discord_name"]:
-                    update_user = True
-                elif member.discriminator != user["discord_discriminator"]:
-                    update_user = True
-                elif member.display_name != user["discord_nickname"]:
-                    update_user = True
-
-                if update_user:
-                    await conn.execute(sql.update_discord_user_names(),
-                                       member.id,
-                                       member.name,
-                                       member.discriminator,
-                                       member.display_name)
-
-
-    @commands.slash_command(auto_sync=True)
-    async def create_tag(self, inter: disnake.CommandInteraction):
-        # Sends a modal using a high level implementation.
-        await inter.response.send_modal(modal=MyModal())
-
-    @commands.slash_command(auto_sync=True)
-    async def create_tag_low(self, inter: disnake.CommandInteraction):
-        # Works same as the above code but using a low level interface.
-        # It's recommended to use this if you don't want to increase cache usage.
-        await inter.response.send_modal(
-            title="Create Tag",
-            custom_id="create_tag_low",
-            components=[
-                disnake.ui.TextInput(
-                    label="Name",
-                    placeholder="The name of the tag",
-                    custom_id="name",
-                    style=disnake.TextInputStyle.short,
-                    max_length=50,
-                ),
-                disnake.ui.TextInput(
-                    label="Description",
-                    placeholder="The description of the tag",
-                    custom_id="description",
-                    style=disnake.TextInputStyle.short,
-                    min_length=5,
-                    max_length=50,
-                ),
-                disnake.ui.TextInput(
-                    label="Content",
-                    placeholder="The content of the tag",
-                    custom_id="content",
-                    style=disnake.TextInputStyle.paragraph,
-                    min_length=5,
-                    max_length=1024,
-                ),
-            ],
-        )
-
-        # Waits until the user submits the modal.
-        try:
-            modal_inter: disnake.ModalInteraction = await bot.wait_for(
-                "modal_submit",
-                check=lambda
-                    i: i.custom_id == "create_tag_low" and i.author.id == inter.author.id,
-                timeout=300,
-            )
-        except asyncio.TimeoutError:
-            # The user didn't submit the modal in the specified period of time.
-            # This is done since Discord doesn't dispatch any event for when a modal is closed/dismissed.
-            return
-
-        embed = disnake.Embed(title="Tag Creation")
-        for custom_id, value in modal_inter.text_values.items():
-            embed.add_field(name=custom_id.capitalize(), value=value,
-                            inline=False)
-        await modal_inter.response.send_message(embed=embed)
-
-
-class MyModal(disnake.ui.Modal):
-    def __init__(self) -> None:
-        components = [
-            disnake.ui.TextInput(
-                label="Name",
-                placeholder="The name of the tag",
-                custom_id="name",
-                style=disnake.TextInputStyle.short,
-                max_length=50,
+    async def test(self, inter):
+        embeds = [
+            disnake.Embed(
+                title="Paginator example",
+                description="This is the first embed.",
+                colour=disnake.Colour.random(),
             ),
-            disnake.ui.TextInput(
-                label="Description",
-                placeholder="The description of the tag",
-                custom_id="description",
-                style=disnake.TextInputStyle.short,
-                min_length=5,
-                max_length=50,
+            disnake.Embed(
+                title="Paginator example",
+                description="This is the second embed.",
+                colour=disnake.Color.random(),
             ),
-            disnake.ui.TextInput(
-                label="Content",
-                placeholder="The content of the tag",
-                custom_id="content",
-                style=disnake.TextInputStyle.paragraph,
-                min_length=5,
-                max_length=1024,
+            disnake.Embed(
+                title="Paginator example",
+                description="This is the third embed.",
+                colour=disnake.Color.random(),
             ),
         ]
-        super().__init__(title="Create Tag", custom_id="create_tag",
-                         components=components)
 
-    async def callback(self, inter: disnake.ModalInteraction) -> None:
-        embed = disnake.Embed(title="Tag Creation")
-        for key, value in inter.text_values.items():
-            embed.add_field(name=key.capitalize(), value=value, inline=False)
-        await inter.response.send_message(embed=embed)
-
-    async def on_error(self, error: Exception,
-                       inter: disnake.ModalInteraction) -> None:
-        await inter.response.send_message("Oops, something went wrong.",
-                                          ephemeral=True)
+        view = Paginator(embeds, 5)
+        await inter.send(embeds=view.embed, view=view)
 
 
+
+    # Creates the embeds as a list.
 def setup(bot):
     bot.add_cog(Tester(bot))
